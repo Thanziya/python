48811445a2deb4ba5bee27123b637db866245cbf
FATHIMA THANZIYA <93600780+Thanziya@users.noreply.github.com>
2021-12-30 20:05:36 +0530

    Update C2-14(SWAP-CHARS).py

--- CYCLE -2/C2-14(SWAP-CHARS).py
+++ CYCLE -2/C2-14(SWAP-CHARS).py
@@ -1,5 +1,5 @@
-str1 = input("Please Enter First String : ")
-str2 =input("Please Enter Second String : ")
+str1 = input(" Enter First String : ")
+str2 =input("Enter Second String : ")
 x=str1[0:1]
 str1=str1.replace(str1[0:1],str2[0:1])
 str2=str2.replace(str2[0:1],x)
